f3f2543dfd33ddefd3da997a59b9e7ba93bbcc1b
Cheng Tan <noinil@gmail.com>
2020-01-23 00:49:18 +0900

    bugfix: missing chain_id and seg_name in GenTopAtom() instance in  parser_top.jl

--- src/lib/parser_top.jl
+++ src/lib/parser_top.jl
@@ -611,7 +611,9 @@ function read_grotop(top_filename::AbstractString)
                                    t.atom_name,
                                    t.function_type,
                                    t.charge,
-                                   t.mass)
+                                   t.mass,
+                                   t.chain_id,
+                                   t.seg_name)
                     push!(top_atoms, s)
                     push!(global_index_2_local_index, t.atom_index)
                     push!(global_index_2_local_molid, mol_id)
