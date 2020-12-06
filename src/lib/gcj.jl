60dc7629a675fa3d615f38cc5af0de99d9238d82
Cheng Tan <noinil@gmail.com>
2020-12-06 20:49:18 +0900

    add an alias file to include all the basic functions.

---
+++ src/lib/gcj.jl
@@ -0,0 +1,12 @@
+include("./biomath.jl")
+include("./constants.jl")
+include("./molecule.jl")
+include("./topology.jl")
+include("./selection.jl")
+include("./conformation.jl")
+include("./parsers.jl")
+include("./coarse_graining_subroutines.jl")
+include("./coarse_graining.jl")
+include("./collective_variables.jl")
+
+
